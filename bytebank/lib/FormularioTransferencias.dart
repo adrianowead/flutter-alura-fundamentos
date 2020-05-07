3b874b47eb46f527fcefb5108df31446928c4669
Adriano Maciel <adriano_mail@hotmail.com>
2020-05-06 21:37:03 -0300

    Convertido formulário para stateful, para evitar bugs ao preencher os campos em landscape

--- bytebank/lib/FormularioTransferencias.dart
+++ bytebank/lib/FormularioTransferencias.dart
@@ -3,35 +3,9 @@ import 'package:flutter/material.dart';
 
 import 'transferencia.dart';
 
-class FormularioTransferencias extends StatelessWidget {
+class FormularioTransferencias extends StatefulWidget {
   final TextEditingController _ctrlCampoNumeroConta = TextEditingController();
   final TextEditingController _ctrlCampoValor = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Criando Transferência'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(children: <Widget>[
-          Editor(
-              ctrl: this._ctrlCampoNumeroConta,
-              rotulo: 'Número da conta',
-              dica: '0000'),
-          Editor(
-              ctrl: this._ctrlCampoValor,
-              rotulo: 'Valor',
-              dica: '0.00',
-              icon: Icons.monetization_on),
-          RaisedButton(
-            child: Text('Confirmar'),
-            onPressed: () => this._criaTransferencia(context),
-          )
-        ]),
-      ),
-    );
-  }
 
   void _criaTransferencia(BuildContext context) {
     final int numeroConta = int.tryParse(this._ctrlCampoNumeroConta.text);
@@ -43,5 +17,38 @@ class FormularioTransferencias extends StatelessWidget {
 
       Navigator.pop(context, transferenciaCriada);
     }
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTransferenciasState();
+  }
+}
+
+class FormularioTransferenciasState extends State<FormularioTransferencias> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando Transferência'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Editor(
+              ctrl: widget._ctrlCampoNumeroConta,
+              rotulo: 'Número da conta',
+              dica: '0000'),
+          Editor(
+              ctrl: widget._ctrlCampoValor,
+              rotulo: 'Valor',
+              dica: '0.00',
+              icon: Icons.monetization_on),
+          RaisedButton(
+            child: Text('Confirmar'),
+            onPressed: () => widget._criaTransferencia(context),
+          )
+        ]),
+      ),
+    );
   }
 }
