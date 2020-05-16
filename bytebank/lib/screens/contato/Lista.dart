efd6591b157e97c71c3a6bcc3927e8553fff45a8
Adriano Maciel <adriano_mail@hotmail.com>
2020-05-16 17:14:36 -0300

    Ajustadas as responsabilidades

--- bytebank/lib/screens/contato/Lista.dart
+++ bytebank/lib/screens/contato/Lista.dart
@@ -1,22 +1,15 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:bytebank/components/CarregandoSpinner.dart';
-import 'package:bytebank/components/ResponseDialog.dart';
 import 'package:bytebank/components/SemConteudo.dart';
-import 'package:bytebank/components/TransferenciaAuthDialog.dart';
 import 'package:bytebank/database/dao/ContatoDao.dart';
-import 'package:bytebank/http/webClients/TransacaoWebClient.dart';
 import 'package:bytebank/models/ItemContato.dart';
-import 'package:bytebank/models/Transferencia.dart';
 import 'package:bytebank/screens/contato/Formulario.dart';
 import 'package:bytebank/screens/transferencias/Formulario.dart';
 import 'package:flutter/material.dart';
-import 'package:http_interceptor/models/http_interceptor_exception.dart';
 
 class ListaContatos extends StatelessWidget {
   final ContatoDao _contatoDao = ContatoDao();
-  final TransacaoWebClient _webClient = TransacaoWebClient();
 
   @override
   Widget build(BuildContext context) {
@@ -62,38 +55,11 @@ class ListaContatos extends StatelessWidget {
                           nome: contato.nome,
                           conta: contato.conta,
                           onClick: () async {
-                            Transferencia transferencia =
-                                await Navigator.of(context).push(
+                            Navigator.of(context).push(
                               MaterialPageRoute(
                                 builder: (context) =>
                                     FormularioTransferencias(contato: contato),
                               ),
-                            );
-
-                            showDialog(
-                              context: context,
-                              builder: (contextAuth) {
-                                return TransferenciaAuthDialog(
-                                  onConfirm: (String passwd) async {
-                                    dynamic salvo = await _savlarOnline(
-                                        transferencia, passwd, context);
-
-                                    if (salvo != null) {
-                                      await showDialog(
-                                        context: context,
-                                        builder: (contextWarning) {
-                                          return SuccessDialog(
-                                            message:
-                                                "Transação salva online com sucesso!",
-                                          );
-                                        },
-                                      );
-
-                                      Navigator.pop(context);
-                                    }
-                                  },
-                                );
-                              },
                             );
                           },
                         ),
@@ -126,45 +92,6 @@ class ListaContatos extends StatelessWidget {
         },
         child: Icon(Icons.add),
       ),
-    );
-  }
-
-  Future _savlarOnline(
-      Transferencia transferencia, String passwd, BuildContext context) async {
-    dynamic salvo = await this
-        ._webClient
-        .save(transferencia, passwd)
-        .catchError(
-          (e) => this
-              ._showFailureMessage(context, 'Sem internet, tempo expirado.'),
-          test: (e) => e is TimeoutException,
-        )
-        .catchError(
-          (e) => this._showFailureMessage(context, 'Sem conexão de internet.'),
-          test: (e) =>
-              e is SocketException ||
-              e is HttpInterceptorException ||
-              e is WebSocketException,
-        )
-        .catchError(
-          (e) => this._showFailureMessage(context, e.message),
-          test: (e) => e is HttpException,
-        )
-        .catchError(
-          (e) =>
-              this._showFailureMessage(context, 'Ocorreu um erro inesperado.'),
-        );
-    return salvo;
-  }
-
-  void _showFailureMessage(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (contextWarning) {
-        return ErrorDialog(
-          message: message,
-        );
-      },
     );
   }
 
